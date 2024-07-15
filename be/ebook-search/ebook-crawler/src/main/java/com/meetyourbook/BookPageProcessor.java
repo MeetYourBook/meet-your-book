3fc7c307ba4ab380f16bf6ad0e51fc26659f3e93
hiidy <hiidy115@gmail.com>
2024-07-15 20:41:28 +0900

    feat: 책 파싱 기능 개선 및 페이지 인덱스에 따른 동적 크롤링 기능 추가
    - 기존 parsing 과정에서 li 태그를 찾지 못하는 문제 해결
    - stream을 통한 null 예외처리 추가
    - 다음 페이지 inde를 target request에 추가해서 동적으로 크롤링 하는 기능 구현

--- be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/BookPageProcessor.java
+++ be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/BookPageProcessor.java
@@ -1,12 +1,12 @@
 package com.meetyourbook;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -22,8 +22,16 @@ public class BookPageProcessor implements PageProcessor {
     private static final String RESULT_LIST_CLASS = "book_resultList";
     private static final String PROVIDER_CLASS = "store";
     private static final String TITLE_CLASS = "tit";
+    private static final String WRITER_CLASS = "writer";
     private static final String NONE_DATA = "";
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+    private static final String IMG_TAG = "img";
+    private static final String SRC_ATTR = "src";
+    private static final String SPAN_TAG = "span";
+    private static final String I_TAG = "i";
+    private static final String BOOK_RESULT_TXT_CLASS = "book_resultTxt";
+    private static final String STRONG_TAG = "strong";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE;
+    private final Site site = Site.me().setTimeOut(10000000);
 
     @Override
     public Site getSite() {
@@ -32,67 +40,159 @@ public class BookPageProcessor implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        Document doc = Jsoup.parse(page.getHtml().toString());
-        Elements bookElements = Objects.requireNonNull(doc.getElementsByClass(RESULT_LIST_CLASS).first()).children();
+        log.info("사이트 이름: {}", getSite().getDomain());
+        Document doc = page.getHtml().getDocument();
 
-        List<BookInfo> books = bookElements.stream()
+        log.info("총 책의 수: {}", getTotalBookCount(doc));
+
+        List<BookInfo> books = parseBooks(doc);
+        log.info("파싱된 책의 개수: {}", books.size());
+
+        try {
+            page.addTargetRequest(fetchNextUrl(page.getUrl().toString()));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private List<BookInfo> parseBooks(Document doc) {
+        Element resultListElement = doc.getElementsByClass(RESULT_LIST_CLASS).first();
+        if (resultListElement == null) {
+            log.warn("Result list element not found");
+            return List.of();
+        }
+
+        Elements bookElements = getBookElements(resultListElement);
+
+        return bookElements.stream()
             .map(this::parseBookInfo)
             .filter(Optional::isPresent)
             .map(Optional::get)
-            .peek(bookInfo -> log.info("bookInfo: {}", bookInfo))
+            .peek(bookInfo -> log.info("book: {}", bookInfo))
             .toList();
+    }
 
+    private Elements getBookElements(Element resultListElement) {
+        Elements bookElements = new Elements();
+        for (Element child : resultListElement.children()) {
+            if (I_TAG.equals(child.tagName())) {
+                bookElements.addAll(child.children());
+            } else {
+                bookElements.add(child);
+            }
+        }
+        return bookElements;
     }
 
     private Optional<BookInfo> parseBookInfo(Element bookElement) {
         try {
-            Element writerElement = bookElement.getElementsByClass("writer").first();
+            Element writerElement = bookElement.getElementsByClass(WRITER_CLASS).first();
+            if (writerElement == null) {
+                log.warn("Writer element not found for book");
+                return Optional.empty();
+            }
+
             return Optional.of(BookInfo.builder()
-                .provider(getProvider(bookElement))
-                .title(getTitle(bookElement))
+                .provider(getTextFromClass(bookElement, PROVIDER_CLASS))
+                .title(getTextFromClass(bookElement, TITLE_CLASS))
                 .author(getAuthor(writerElement))
                 .publisher(getPublisher(writerElement))
                 .publishDate(getPublishDate(writerElement))
                 .imageURL(getImgUrl(bookElement))
                 .build());
         } catch (Exception e) {
-            log.error("parseBookInfo error", e);
+            log.error("Error parsing book info", e);
             return Optional.empty();
         }
     }
 
+    private int getTotalBookCount(Document document) {
+        Element bookResultTxt = document.getElementsByClass(BOOK_RESULT_TXT_CLASS).first();
+        if (bookResultTxt == null) {
+            log.warn("BookResultText가 존재하지 않습니다.");
+            return 0;
+        }
+
+        Element strongElement = bookResultTxt.getElementsByTag(STRONG_TAG).first();
+        if (strongElement == null) {
+            log.warn("BookResultText에서 Strong태그 Element가 없음");
+            return 0;
+        }
+
+        String countText = strongElement.text().replaceAll("\\D", "");
+        try {
+            return Integer.parseInt(countText);
+        } catch (NumberFormatException e) {
+            log.error("책의 총개수를 파싱하는 과정에서 오류가 생김", e);
+            return 0;
+        }
+    }
 
     private String getTextFromClass(Element element, String className) {
-        Element targetElement = element.getElementsByClass(className).first();
-        if (targetElement != null) {
-            return targetElement.text();
-        }
-        return NONE_DATA;
-    }
-
-    private String getProvider(Element element) {
-        return getTextFromClass(element, PROVIDER_CLASS);
-    }
-
-    private String getTitle(Element element) {
-        return getTextFromClass(element, TITLE_CLASS);
+        return Optional.ofNullable(element.getElementsByClass(className).first())
+            .map(Element::text)
+            .orElse(NONE_DATA);
     }
 
     private String getAuthor(Element writerElement) {
-        return writerElement.textNodes().get(0).text();
+        return writerElement.textNodes().stream()
+            .findFirst()
+            .map(node -> node.text().trim())
+            .orElse(NONE_DATA);
     }
 
     private String getPublisher(Element writerElement) {
-        return writerElement.getElementsByTag("span").first().text().trim();
+        return Optional.ofNullable(writerElement.getElementsByTag(SPAN_TAG).first())
+            .map(element -> element.text().trim())
+            .orElse(NONE_DATA);
     }
 
     private LocalDate getPublishDate(Element writerElement) {
-        String publishDate = writerElement.textNodes().get(1).text();
-        return LocalDate.parse(publishDate, DateTimeFormatter.ISO_LOCAL_DATE);
+        return writerElement.textNodes().stream()
+            .skip(1)
+            .findFirst()
+            .map(node -> LocalDate.parse(node.text(), DATE_FORMATTER))
+            .orElse(null);
     }
 
     private String getImgUrl(Element element) {
-        return element.getElementsByTag("img").first().attr("src").substring(2);
+        return Optional.ofNullable(element.getElementsByTag(IMG_TAG).first())
+            .map(img -> img.attr(SRC_ATTR))
+            .map(url -> url.startsWith("//") ? url.substring(2) : url)
+            .orElse(NONE_DATA);
+    }
+
+    private String fetchNextUrl(String url) throws URISyntaxException {
+        URI uri = URI.create(url);
+        String query = uri.getQuery();
+        String[] params = query.split("&");
+        StringBuilder newQuery = new StringBuilder();
+
+        for (String param : params) {
+            if (param.startsWith("pageIndex=")) {
+                String[] keyValue = param.split("=");
+                int pageIndex = Integer.parseInt(keyValue[1]);
+                pageIndex++;
+                newQuery.append("pageIndex=").append(pageIndex).append("&");
+            } else {
+                newQuery.append(param).append("&");
+            }
+        }
+
+        if (!newQuery.isEmpty()) {
+            newQuery.setLength(newQuery.length() - 1);
+        }
+
+        URI newUri = new URI(
+            uri.getScheme(),
+            uri.getAuthority(),
+            uri.getPath(),
+            newQuery.toString(),
+            uri.getFragment()
+        );
+
+        return newUri.toString();
     }
 
 

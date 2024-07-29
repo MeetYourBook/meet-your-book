2e7ed25ea99fa258c9c349b8fe5c87e64e14e147
Keon Kim <fnc6488@gmail.com>
2024-07-30 00:24:09 +0900

    feat: BookService 클래스 변경
    - Library 조회 시 캐싱 기능 추가
    - 반복문을 stream으로 변경

--- be/ebook-search/ebook-core/src/main/java/com/meetyourbook/service/BookService.java
+++ be/ebook-search/ebook-core/src/main/java/com/meetyourbook/service/BookService.java
@@ -4,13 +4,14 @@ import com.meetyourbook.dto.BookInfo;
 import com.meetyourbook.entity.Book;
 import com.meetyourbook.entity.Library;
 import com.meetyourbook.repository.BookRepository;
-import jakarta.transaction.Transactional;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,28 +21,30 @@ public class BookService {
     private final LibraryService libraryService;
     private final BookRepository bookRepository;
 
-
-
     @Transactional
     public void saveAll(List<BookInfo> bookInfos) {
+        log.info("Saving {} books", bookInfos.size());
+        Map<String, Library> libraryCache = new HashMap<>();
 
-        List<Book> books = new ArrayList<>();
-        for (BookInfo bookInfo : bookInfos) {
-            Book book = findOrCreateBook(bookInfo);
-            Library library = libraryService.findByBaseUrl(bookInfo.getBaseUrl());
-            book.addLibrary(library);
-            books.add(book);
-        }
+        List<Book> booksToSave = bookInfos.stream()
+            .map(bookInfo -> processBookInfo(bookInfo, libraryCache)).toList();
 
-        bookRepository.saveAll(books);
+        bookRepository.saveAll(booksToSave);
     }
 
-    private Book findOrCreateBook(BookInfo bookInfo) {
-        Optional<Book> optionalBook = bookRepository.findFirstByTitleAndAuthorAndPublisherAndPublishDate(
+    private Book processBookInfo(BookInfo bookInfo, Map<String, Library> libraryCache) {
+        Library library = libraryCache.computeIfAbsent(bookInfo.getBaseUrl(),
+            libraryService::findByBaseUrl);
+
+        Book book = findBookByBookInfo(bookInfo).orElseGet(bookInfo::toEntity);
+        book.addLibrary(library);
+        return book;
+    }
+
+    private Optional<Book> findBookByBookInfo(BookInfo bookInfo) {
+        return bookRepository.findFirstByTitleAndAuthorAndPublisherAndPublishDate(
             bookInfo.getTitle(), bookInfo.getAuthor(), bookInfo.getPublisher(),
             bookInfo.getPublishDate());
-
-        return optionalBook.orElseGet(bookInfo::toEntity);
     }
 
 }

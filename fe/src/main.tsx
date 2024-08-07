c540bf6d3379ade6d9266ca227a193977a3fbd68
임승현 <96limshyun@gmail.com>
2024-08-07 19:38:19 +0900

    chore: msw setup
    - msw setup
    - dev에서만 실행하도록 설정
    
    Closes #14

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './routers/routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routers/routes.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>,
-)
+const main = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./mocks/node.ts');
+    await worker.start();
+  }
+};
+await main();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={routes} />
+    </React.StrictMode>
+);
--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
 
 const Home = () => {
+    async function app() {
+        const response = await fetch('/test')
+        const user = await response.json()
+        console.log(user)
+    }
+    useEffect(() => {
+        app()
+    }, []);
+
     return <div>Home</div>;
 };
 

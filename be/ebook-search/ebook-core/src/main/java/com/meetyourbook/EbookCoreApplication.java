d168b5066b85b78dfb5d861a8624d4fed8aa5384
Keon Kim <fnc6488@gmail.com>
2024-08-07 18:10:48 +0900

    feat: API 서버 메인 애플리케이션 추가

---
+++ be/ebook-search/ebook-core/src/main/java/com/meetyourbook/EbookCoreApplication.java
@@ -0,0 +1,13 @@
+package com.meetyourbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbookCoreApplication.class, args);
+    }
+
+}

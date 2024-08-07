3675dd924a12640433306e536da69c81925f7fb1
임승현 <96limshyun@gmail.com>
2024-08-07 14:42:43 +0900

    init: 프로젝트 초기설정
    - 라우터 분리
    - styled-component 설정

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,18 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from 'react-query/devtools';
-import routes from "./routers/routes.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import routes from './routers/routes.tsx'
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={routes} />
-            <ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
-        </QueryClientProvider>
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>,
+)

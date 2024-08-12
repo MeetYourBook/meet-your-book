dd7b85f10e8b6c6f2b995b8fe4b9f2ca8878b4dd
Keon Kim <fnc6488@gmail.com>
2024-08-13 03:19:07 +0900

    feat: 책 생성, 삭제, 업데이트 API 구현
    - RecourseNotFoundException 예외 정의
    - 제목, 저자, 출판사, 유통사는 `@NotBlank` 검증

---
+++ be/ebook-search/ebook-core/src/main/java/com/meetyourbook/dto/BookUpdateRequest.java
@@ -0,0 +1,17 @@
+package com.meetyourbook.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import lombok.Builder;
+
+@Builder
+public record BookUpdateRequest(
+    @NotBlank(message = "Title is required") String title,
+    @NotBlank(message = "Author is required") String author,
+    @NotBlank(message = "Publisher is required") String publisher,
+    @NotBlank(message = "Provider is required") String provider,
+    LocalDate publishDate,
+    String imageUrl
+) {
+
+}
--- be/ebook-search/ebook-core/src/main/java/com/meetyourbook/service/BookService.java
+++ be/ebook-search/ebook-core/src/main/java/com/meetyourbook/service/BookService.java
@@ -1,11 +1,15 @@
 package com.meetyourbook.service;
 
+import com.meetyourbook.dto.BookCreateRequest;
 import com.meetyourbook.dto.BookPageResponse;
 import com.meetyourbook.dto.BookSearchRequest;
+import com.meetyourbook.dto.BookUpdateRequest;
 import com.meetyourbook.entity.Book;
+import com.meetyourbook.exception.ResourceNotFoundException;
 import com.meetyourbook.repository.BookRepository;
 import com.meetyourbook.spec.BookSpecs;
 import com.meetyourbook.spec.SpecBuilder;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,6 +30,30 @@ public class BookService {
         return BookPageResponse.of(bookPage);
     }
 
+    @Transactional
+    public UUID createBook(BookCreateRequest request) {
+        Book book = request.toEntity();
+        bookRepository.save(book);
+        return book.getId();
+    }
+
+    @Transactional
+    public void deleteBook(UUID id) {
+        Book book = findBook(id);
+        bookRepository.delete(book);
+    }
+
+    @Transactional
+    public void updateBook(UUID bookId, BookUpdateRequest request) {
+        Book book = findBook(bookId);
+        book.update(request);
+    }
+
+    private Book findBook(UUID id) {
+        return bookRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Book not found with id: " + id));
+    }
+
     private Specification<Book> createBookSpec(BookSearchRequest request) {
         return SpecBuilder.builder(Book.class)
             .ifNotNullOr(request.title(), BookSpecs::titleContains)
@@ -34,5 +62,4 @@ public class BookService {
             .ifNotNullAnd(request.libraries(), BookSpecs::inLibraries)
             .toSpec();
     }
-
 }

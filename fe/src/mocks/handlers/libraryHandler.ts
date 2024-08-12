886514d75c48206ecc3986721d66a83723ee8c67
임승현 <96limshyun@gmail.com>
2024-08-12 17:18:25 +0900

    refactor: API 데이터 변경에 따른 타입 업데이트

--- fe/src/mocks/handlers/libraryHandler.ts
+++ fe/src/mocks/handlers/libraryHandler.ts
@@ -2,7 +2,7 @@ import {http, HttpResponse} from "msw";
 import libraryData from "../mockData/library.json"
 
 export const libraryHandlers = [
-    http.get("api/library", () => {
+    http.get("api/libraries", () => {
         return HttpResponse.json(libraryData.library)
     })
 ]

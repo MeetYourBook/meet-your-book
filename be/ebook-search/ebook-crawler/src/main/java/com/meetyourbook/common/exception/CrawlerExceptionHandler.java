2a2eba0ed626f91c2eb0eadfae2b8e4d639e2ea9
Keon Kim <fnc6488@gmail.com>
2024-08-19 19:08:49 +0900

    feat: JSON 파일을 업로드하지 않을 경우 발생하는 예외 처리 메서드 추가

--- be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/common/exception/CrawlerExceptionHandler.java
+++ be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/common/exception/CrawlerExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @Slf4j
 @RestControllerAdvice
@@ -42,6 +43,15 @@ public class CrawlerExceptionHandler {
     @ExceptionHandler(JsonParseException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<CrawlerErrorResponse> handleJsonParseException(
+        CrawlerNotRunningException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+            new CrawlerErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage(),
+                LocalDateTime.now()));
+    }
+
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<CrawlerErrorResponse> handleMissingServletRequestPartException(
         CrawlerNotRunningException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
             new CrawlerErrorResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage(),

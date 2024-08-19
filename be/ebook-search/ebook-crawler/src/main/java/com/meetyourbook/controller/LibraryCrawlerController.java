1362d9545539110aa9b0dd9bebed2780c1383853
Keon Kim <fnc6488@gmail.com>
2024-08-19 19:06:25 +0900

    feat: @RequestParam 을 @RequestPart 로 변경

--- be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/controller/LibraryCrawlerController.java
+++ be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/controller/LibraryCrawlerController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,7 +18,7 @@ public class LibraryCrawlerController {
     private final LibraryImportService libraryImportService;
 
     @PostMapping("/import")
-    public ResponseEntity<?> saveLibraryFromJson(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<?> saveLibraryFromJson(@RequestPart("file") MultipartFile file) {
         int importedCount = libraryImportService.importLibrariesFromJson(file);
         return ResponseEntity.ok(new ImportResult(importedCount, "도서관 정보를 성공적으로 저장했습니다."));
     }

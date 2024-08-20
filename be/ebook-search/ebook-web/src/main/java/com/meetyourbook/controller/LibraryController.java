10996c23d5153823fb2e9aaa37c1486ca18ba2eb
hiidy <hiidy115@gmail.com>
2024-08-21 01:38:17 +0900

    refactor: 경로 변수명 명확화, update 요청을 patch로 변경
    - 모든 엔드포인트에서 'id'를 'libraryId'로 변경
    - 기존 update 요청이 `PUT`메서드였지만 이를 `PATCH`로 변경했습니다.

--- be/ebook-search/ebook-web/src/main/java/com/meetyourbook/controller/LibraryController.java
+++ be/ebook-search/ebook-web/src/main/java/com/meetyourbook/controller/LibraryController.java
@@ -10,9 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,32 +29,30 @@ public class LibraryController {
         return libraryWebService.findAllLibraryResponses();
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<LibraryResponse> getLibrary(@PathVariable long id) {
-        LibraryResponse response = libraryWebService.findById(id);
+    @GetMapping("/{libraryId}")
+    public ResponseEntity<LibraryResponse> getLibrary(@PathVariable Long libraryId) {
+        LibraryResponse response = libraryWebService.findById(libraryId);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping
     public ResponseEntity<Void> createLibrary(@RequestBody LibraryCreateRequest request) {
-        Long id = libraryWebService.createLibrary(request);
-        URI location = URI.create("/api/libraries/" + id);
+        Long libraryId = libraryWebService.createLibrary(request);
+        URI location = URI.create("/api/libraries/" + libraryId);
         return ResponseEntity.created(location).build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<LibraryResponse> updateLibrary(@PathVariable Long id,
+    @PatchMapping("/{libraryId}")
+    public ResponseEntity<LibraryResponse> updateLibrary(@PathVariable Long libraryId,
         @RequestBody LibraryUpdateRequest request) {
-
-        LibraryResponse response = libraryWebService.updateLibrary(id, request);
+        LibraryResponse response = libraryWebService.updateLibrary(libraryId, request);
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteLibrary(@PathVariable Long id) {
-        libraryWebService.delete(id);
+    @DeleteMapping("/{libraryId}")
+    public ResponseEntity<Void> deleteLibrary(@PathVariable Long libraryId) {
+        libraryWebService.delete(libraryId);
         return ResponseEntity.noContent().build();
-
     }
 
 }

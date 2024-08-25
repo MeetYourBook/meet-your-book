be6367bf63a0988dac1d4a1bd676076c3c13baa9
임승현 <96limshyun@gmail.com>
2024-08-25 17:49:20 +0900

    feat: 도서관 정보 검색기능 구현
    - useDebounce를 재활용해 검색한 뒤 0.3초 뒤 검색 결과가 나오도록 구현

--- fe/src/components/BookInfoModal/LibrariesPaging.tsx
+++ fe/src/components/BookInfoModal/LibrariesPaging.tsx
@@ -1,16 +1,35 @@
 import { LibrariesType } from "@/types/Libraries";
 import * as S from "@/styles/LibrariesPagingStyle";
+import { useMemo, useState } from "react";
+import useDebounce from "@/hooks/useDebounce";
+import { DEBOUNCE_TIME } from "@/constants";
 interface LibrariesPagingProps {
     libraryResponses: LibrariesType[];
 }
 const LibrariesPaging = ({ libraryResponses }: LibrariesPagingProps) => {
+    const [searchValue, setSearchValue] = useState("");
+
+    const debouncedValue = useDebounce(searchValue, DEBOUNCE_TIME);
+    
+    const filteredLibraries = useMemo(() => {
+        return debouncedValue
+        ? libraryResponses.filter((item: LibrariesType) => item.LibraryName.includes(debouncedValue))
+        : libraryResponses;
+    }, [debouncedValue, libraryResponses]);
+
     return (
         <S.Container>
             <p>소장 도서관: {libraryResponses.length}</p>
-            <S.Input placeholder="도서관 검색..." />
+            <S.Input
+                value={searchValue}
+                placeholder="도서관 검색..."
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
             <S.LibrariesWrap>
-                {libraryResponses.map((curLibrary, idx) => (
-                    <S.LibraryItem key={idx}>{curLibrary.LibraryName}</S.LibraryItem>
+                {filteredLibraries.map((curLibrary, idx) => (
+                    <S.LibraryItem key={idx}>
+                        {curLibrary.LibraryName}
+                    </S.LibraryItem>
                 ))}
             </S.LibrariesWrap>
         </S.Container>

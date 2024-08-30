5c0515f30ed3a058922ebacda1189048e2d508be
hiidy <hiidy115@gmail.com>
2024-08-30 17:36:10 +0900

    feat: LibraryCreationInfo에서 category 삭제

--- be/ebook-search/ebook-domain/src/main/java/com/meetyourbook/dto/LibraryCreationInfo.java
+++ be/ebook-search/ebook-domain/src/main/java/com/meetyourbook/dto/LibraryCreationInfo.java
@@ -1,12 +1,10 @@
 package com.meetyourbook.dto;
 
 import com.meetyourbook.entity.Library;
-import com.meetyourbook.entity.Library.LibraryType;
 import lombok.Builder;
 
 @Builder
 public record LibraryCreationInfo(
-    String category,
     String press,
     String url
 ) {
@@ -14,7 +12,6 @@ public record LibraryCreationInfo(
     public Library toEntity() {
         return Library.builder()
             .name(press)
-            .type(LibraryType.findByDescription(category))
             .libraryUrl(url)
             .build();
     }

ccf1988137087667476d97b350f15a5fcb06f68f
Keon Kim <fnc6488@gmail.com>
2024-08-31 16:38:40 +0900

    refactor: BookLibraryService 리팩터링
    - crawler 모듈로 이동
    - 캐시를 Spring Cache 로 변경

---
+++ be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/service/BookCacheService.java
@@ -0,0 +1,28 @@
+package com.meetyourbook.service;
+
+import com.meetyourbook.dto.BookRecord;
+import com.meetyourbook.dto.BookUniqueKey;
+import com.meetyourbook.repository.BookJdbcRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class BookCacheService {
+
+    private final BookJdbcRepository bookJdbcRepository;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Cacheable(value = "bookCache", key = "#bookUniqueKey", sync = true)
+    public UUID getBookIdFromCache(BookUniqueKey bookUniqueKey, BookRecord bookRecord) {
+        return bookJdbcRepository.findIdByUniqueKey(bookUniqueKey)
+            .orElseGet(() -> bookJdbcRepository.saveBook(bookRecord));
+    }
+
+}
---
+++ be/ebook-search/ebook-crawler/src/main/java/com/meetyourbook/service/LibraryCacheService.java
@@ -0,0 +1,24 @@
+package com.meetyourbook.service;
+
+import com.meetyourbook.entity.Library;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LibraryCacheService {
+
+    private final LibraryDomainService libraryDomainService;
+
+    @Transactional(readOnly = true)
+    @Cacheable(value = "libraryCache", key = "#baseUrl", sync = true)
+    public Long getLibraryFromCache(String baseUrl) {
+        Library library = libraryDomainService.findByBaseUrl(baseUrl);
+        return library.getId();
+    }
+
+}

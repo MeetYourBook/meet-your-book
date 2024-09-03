ac82bf36f5601891dfd277191b24ac36ecd770a1
hiidy <hiidy115@gmail.com>
2024-09-03 14:29:05 +0900

    fix: MainInk페이지 url을 파싱하지 못한 문제 해결
    - T3_Mobile인지 url에 T3가 있으면 제외했습니다.

--- be/ebook-search/ebook-domain/src/main/java/com/meetyourbook/dto/BookSearchInfo.java
+++ be/ebook-search/ebook-domain/src/main/java/com/meetyourbook/dto/BookSearchInfo.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 @Builder
 public record BookSearchInfo(
@@ -19,7 +20,7 @@ public record BookSearchInfo(
 ) {
 
     public Pageable toPageable() {
-        Sort sort = Sort.by(this.sort);
+        Sort sort = Sort.by(Direction.DESC, this.sort);
         return PageRequest.of(page, size, sort);
     }
 

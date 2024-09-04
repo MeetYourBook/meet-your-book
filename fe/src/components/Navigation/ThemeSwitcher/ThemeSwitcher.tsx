c7bf6eaca96111c18bb542b71c53e4b9ac4cde48
임승현 <96limshyun@gmail.com>
2024-09-05 00:32:17 +0900

    docs: 크로메틱 배포 브랜치 변경 및 불필요한 코드 삭제

--- fe/src/components/Navigation/ThemeSwitcher/ThemeSwitcher.tsx
+++ fe/src/components/Navigation/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,11 +1,8 @@
 import { SunOutlined, MoonOutlined } from "@ant-design/icons";
 import styled from "styled-components";
 import useThemeContext from "@/hooks/useThemeContext";
-import { useEffect } from "react";
 const ThemeSwitcher = () => {
     const { theme, handleToggleTheme } = useThemeContext();
-    useEffect(() => {
-    }, [theme])
     return (
         <span onClick={handleToggleTheme}>
             {theme === "light" ? <LightBtn/> : <DarkBtn/>}

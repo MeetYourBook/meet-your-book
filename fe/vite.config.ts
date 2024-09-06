00d158f84b6eccc47005d499f147e92ad44b6a44
임승현 <96limshyun@gmail.com>
2024-09-06 17:24:08 +0900

    fix: 로그인, 회원가입 스토리북 삭제 및 프록시 타겟 url 환경변수로 수정

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,43 +1,47 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/dev': {
-        target: 'https://api.meetyourbook.site',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/dev/, ''),
-        secure: false,
-        ws: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/dev': {
+          target: env.VITE_ORIGINAL_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/dev/, ''),
+          secure: false,
+          ws: true
+        }
       }
-    }
-  },
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      external: ['msw-storybook-addon'],
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts',
-    coverage: {
-      reporter: ['text', 'lcov'],
-      reportsDirectory: 'coverage',
-      include: [
-        'src/pages/**/*.{ts,tsx}',
-        'src/hooks/**/*.{ts,tsx}',
-        'src/components/**/*.{ts,tsx}',
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, 'src') },
       ],
     },
-  },
+    build: {
+      target: 'esnext',
+      rollupOptions: {
+        external: ['msw-storybook-addon'],
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/tests/setup.ts',
+      coverage: {
+        reporter: ['text', 'lcov'],
+        reportsDirectory: 'coverage',
+        include: [
+          'src/pages/**/*.{ts,tsx}',
+          'src/hooks/**/*.{ts,tsx}',
+          'src/components/**/*.{ts,tsx}',
+        ],
+      },
+    },
+  };
 });

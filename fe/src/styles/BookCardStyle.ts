096d7c79d594996a44609e92b9517a779ee2daeb
임승현 <96limshyun@gmail.com>
2024-09-14 18:05:39 +0900

    style: 리스트보기 레이아웃 수정

--- fe/src/styles/BookCardStyle.ts
+++ fe/src/styles/BookCardStyle.ts
@@ -21,7 +21,6 @@ const Card = styled.div<{ $isVisible: boolean }>`
     }
 `;
 
-
 const GridCard = styled(Card)`
     width: 150px;
     text-align: center;
@@ -40,11 +39,10 @@ const Image = styled.img`
 `;
 
 const TextContainer = styled.div<{ $viewMode: ViewType }>`
-    width: ${({ $viewMode }) =>
-        $viewMode === "list" ? "70%" : "100%"};
-    padding-left: ${({ $viewMode }) =>
-        $viewMode === "list" ? "1rem" : "0"};
-    text-align: ${({ $viewMode }) => ($viewMode === "list" ? "left" : "center")};
+    width: ${({ $viewMode }) => ($viewMode === "list" ? "70%" : "100%")};
+    padding-left: ${({ $viewMode }) => ($viewMode === "list" ? "1rem" : "0")};
+    text-align: ${({ $viewMode }) =>
+        $viewMode === "list" ? "left" : "center"};
     margin: auto 0;
 `;
 
@@ -58,10 +56,10 @@ const Title = styled.h3`
 `;
 
 const ListTitle = styled.h3`
-    font-weight: bold;
-    font-size: 0.875rem;
-    margin: 0.25rem 0;
-`
+    font-size: 1.125rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+`;
 
 const Subtitle = styled.p`
     font-size: 0.75rem;
@@ -72,15 +70,36 @@ const Subtitle = styled.p`
 `;
 
 const MetaInfo = styled.p`
-    font-size: 0.75rem;
-    color: #6b7280;
+    font-size: 0.875rem;
+    color: #4b5563;
+    margin-bottom: 0.25rem;
 `;
 
 const FavoritesBtnWrap = styled.span`
     position: absolute;
     right: 10px;
     z-index: 10;
-`
+`;
+
+const ListBookInfo = styled.div`
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    margin-left: 1rem;
+    justify-content: center;
+`;
+
+const LibrariesCount = styled.div`
+    width: 100px;
+    padding: 5px;
+    font-size: 0.7rem;
+    margin-top: 0.5rem;
+    border: none;
+    background-color: gainsboro;
+    color: black;
+    border-radius: 8px;
+    text-align: center;
+`;
 
 export {
     GridCard,
@@ -91,5 +110,7 @@ export {
     MetaInfo,
     Image,
     FavoritesBtnWrap,
-    ListTitle
+    ListTitle,
+    ListBookInfo,
+    LibrariesCount,
 };
